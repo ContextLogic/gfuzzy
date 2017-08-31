447f393aef79413f5426c23047623d73684eb43b
Wan Hu <wan@contextlogic.com>
2017-08-31 15:17:04 -0700

    Update __init__

---
+++ gfuzzy/__init__.py
@@ -0,0 +1,7 @@
+from gfuzzy import \
+        membership, fuzzy_pb2, variable, defuzzify, consequent, sys_input, \
+        sys_output, rule, system
+
+__all__ = [
+        'membership', 'fuzzy_pb2', 'variable', 'defuzzify',
+        'consequent', 'sys_input', 'sys_output', 'rule', 'system']
