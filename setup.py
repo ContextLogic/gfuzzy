131228ba5e845cc895bf771cc1528d07c7349e68
Wan Hu <wan@contextlogic.com>
2017-08-31 14:54:32 -0700

    Make gfuzzy a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gfuzzy',
+      version='0.1',
+      description='Fuzzy Logic inference engine written in python',
+      url='https://github.com/ContextLogic/gfuzzy',
+      author='Johan Mathe',
+      author_email='',
+      license='Apache 2.0',
+      packages=['gfuzzy'],
+      zip_safe=False)
